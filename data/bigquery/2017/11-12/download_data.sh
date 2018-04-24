caf735e2c95197862b7f7464b99ba0ec1e83f48d
George Ho <georgeho1618@gmail.com>
2018-04-24 16:30:09 -0400

    swap out data

---
+++ data/bigquery/2017/11-12/download_data.sh
@@ -0,0 +1,4 @@
+wget https://storage.googleapis.com/reddit_eigenfoo/2017/11-12/CringeAnarchy.csv
+wget https://storage.googleapis.com/reddit_eigenfoo/2017/11-12/TheRedPill.csv
+wget https://storage.googleapis.com/reddit_eigenfoo/2017/11-12/The_Donald.csv
+
