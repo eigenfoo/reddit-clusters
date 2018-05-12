0306c4bccb1bfbdd00a3b3b077c905129e7f20a1
George Ho <georgeho1618@gmail.com>
2018-05-11 23:13:37 -0400

    ENH added full logs

--- clustering/run_nmfs.sh
+++ clustering/run_nmfs.sh
@@ -1,3 +1,3 @@
-nohup python -u tfidf_nmf.py TheRedPill 20 > log_TheRedPill.txt &
-nohup python -u tfidf_nmf.py The_Donald 20 > log_The_Donald.txt &
-nohup python -u tfidf_nmf.py CringeAnarchy 20 > log_CringeAnarchy.txt &
+nohup python -u tfidf_nmf.py TheRedPill 20 > results/log_TheRedPill.txt &
+nohup python -u tfidf_nmf.py The_Donald 20 > results/log_The_Donald.txt &
+nohup python -u tfidf_nmf.py CringeAnarchy 20 > results/log_CringeAnarchy.txt &
