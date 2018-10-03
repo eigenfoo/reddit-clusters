52897511da693c3a749ecadc58f746798e2259c5
George Ho <georgeho1618@gmail.com>
2018-10-03 16:37:04 -0400

    ENH: added tfidf logic to pmf

--- clustering/pmf/pmf.py
+++ clustering/pmf/pmf.py
@@ -1,28 +1,42 @@
 '''
-Probabilisitic Matrix Factorization using Tensorflow
+Probabilisitic Matrix Factorization (PMF) using Tensorflow.
+
+Original paper:
+    http://papers.nips.cc/paper/3208-probabilistic-matrix-factorization.pdf
 '''
 
 import numpy as np
 import tensorflow as tf
 from scipy import sparse
+import spacy
 
 np.random.seed(1618)
+tf.set_random_seed(1618)
 
-NUM_ITEMS = 5000
-NUM_USERS = 100
-RANK = 10
-NUM_EPOCHS = 50000
-
-
-def build_toy_dataset(noise_std=0.1):
-    U = sparse.random(RANK, NUM_USERS, 0.05)
-    V = sparse.random(RANK, NUM_ITEMS, 0.05)
-    R = U.transpose() * V
-    nonzero_rows, nonzero_cols, nonzero_vals = sparse.find(R)
-    return U, V, R, nonzero_rows, nonzero_cols, nonzero_vals
+DATA_FILE = 'NeutralPolitics.csv'
+NUM_TOKENS = 5000
+NUM_DOCUMENTS = 100
+NUM_LATENTS = 20
+NUM_EPOCHS = 2000
 
 
-def variable_summaries(var, name):
+def mask(token):
+    # Helper function to mask out non-tokens
+    if (not token.is_ascii
+            or token.is_stop
+            or token.like_num
+            or token.pos_ in ['X', 'SYM']):
+        return False
+    return True
+
+
+def tokenize(document):
+    # Tokenize by lemmatizing
+    doc = nlp(document)
+    return [token.lemma_ for token in doc if mask(token)]
+
+
+def variable_summaries(name, var):
     """ Attach summaries to a Tensor (for TensorBoard visualization). """
     with tf.name_scope(name):
         mean = tf.reduce_mean(var)
@@ -37,29 +51,43 @@ def variable_summaries(var, name):
         tf.summary.histogram('histogram', var)
 
 
-U_true, V_true, R_true, nonzero_rows, nonzero_cols, nonzero_vals = \
-    build_toy_dataset()
+# Disable tagger, parser and named-entity recognition
+nlp = spacy.load('en', disable=['tagger', 'parser', 'ner'])
 
+# Read data
+data = pd.read_csv(DATA_FILE).squeeze()
+
+# Vectorize data with tf-idfs
+vectorizer = TfidfVectorizer(strip_accents='unicode',
+                             tokenizer=tokenize,
+                             max_df=0.90,
+                             min_df=0.001,
+                             norm='l2')
+
+tfidf = vectorizer.fit_transform(data)
+
+# Get portions of tf-idf matrix that are nonzero
 index = np.vstack([nonzero_rows, nonzero_cols]).T
-nonzero_R_true = R_true[nonzero_rows, nonzero_cols]
+nonzero_tfidf = tfidf[nonzero_rows, nonzero_cols]
 
-sess = tf.Session()
-
+# Define matrices
 with tf.name_scope('matrices'):
-    U = tf.get_variable('U', [RANK, NUM_USERS], tf.float32,
+    U = tf.get_variable('U', [NUM_LATENTS, NUM_DOCUMENTS], tf.float32,
                         tf.truncated_normal_initializer(mean=0.0, stddev=0.2))
-    V = tf.get_variable('V', [RANK, NUM_ITEMS], tf.float32,
+    V = tf.get_variable('V', [NUM_LATENTS, NUM_TOKENS], tf.float32,
                         tf.truncated_normal_initializer(mean=0.0, stddev=0.2))
     R = tf.matmul(tf.transpose(U), V)
 
-variable_summaries(U, name='U')
-variable_summaries(V, name='V')
-variable_summaries(R, name='R')
+variable_summaries('U', U)
+variable_summaries('V', V)
+variable_summaries('R', R)
 
+# Define loss
 with tf.name_scope('loss'):
-    lambda_U = 0.1
-    lambda_V = 0.1
-    error = tf.reduce_sum((nonzero_R_true - tf.gather_nd(R, index))**2)
+    # TODO regularization parameters may need tuning...
+    lambda_U = 1 / (NUM_LATENTS * NUM_DOCUMENTS)
+    lambda_V = 1 / (NUM_LATENTS * NUM_TOKENS)
+    error = tf.reduce_sum((nonzero_tfidf - tf.gather_nd(R, index))**2)
     regularization_U = lambda_U * tf.reduce_sum(tf.norm(U, axis=1))
     regularization_V = lambda_V * tf.reduce_sum(tf.norm(V, axis=1))
     loss = error + regularization_U + regularization_V
@@ -69,12 +97,14 @@ with tf.name_scope('loss'):
     tf.summary.scalar('regularization_V', regularization_V)
     tf.summary.scalar('loss', loss)
 
+# Define training
 with tf.name_scope('train'):
     train_step = tf.train.AdamOptimizer().minimize(loss)
 
 merged_summary = tf.summary.merge_all()
 writer = tf.summary.FileWriter('./logs', sess.graph)
 
+sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 for i in range(NUM_EPOCHS):
     _, summary_, loss_ = sess.run([train_step, merged_summary, loss])
